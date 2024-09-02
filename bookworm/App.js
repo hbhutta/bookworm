ea47c2a8c3e376e5e8819e7c56a40a8e96f4df25
Haad Bhutta <haadbhutta@gmail.com>
2024-09-01 21:34:04 -0700

    Played around with expo camera

--- bookworm/App.js
+++ bookworm/App.js
@@ -1,11 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { decodeSingle } from "quagga";
+import { Camera } from "expo-camera";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>welcome to bookworm!</Text>
     </View>
   );
 }
@@ -13,8 +15,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
   },
 });
